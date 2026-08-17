a1d93162b2b588a48f517853effc4dfeee805de4
Yaggo Rodrigues de Lima <yaggo.jose.lurdinha1982@gmail.com>
2026-08-17 19:49:30 -0300

    Atividade sobre equação - 17/08/2026
    Tarefa requisitada pelo professor Oldair Gabriel.

---
+++ equacao2.py
@@ -0,0 +1,15 @@
+# A diferença entre um número e sua terça parte é 20. Qual é esse número?
+
+# A diferença entre um número e sua parte é 20.
+# x - (x / 3 ) = 20
+# (3x - x) / 3 = 20
+# 2x = 60
+# x = 30
+
+x = 30
+terca_parte = x / 3
+resultado = x - terca_parte
+
+print(f"O número é {x}.")
+print(f"Sua terça parte é {terca_parte}. ")
+print(f"A diferença entre {x} e {terca_parte} é {resultado}. ")
