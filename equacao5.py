7fac2c9096669cc44a78573f7521c52fa16c4a70
Yaggo Rodrigues de Lima <yaggo.jose.lurdinha1982@gmail.com>
2026-08-17 20:23:52 -0300

    Atividade sobre equação - 17/08/2026
    Atividade requisitada pelo professor Oldair Gabriel.

---
+++ equacao5.py
@@ -0,0 +1,16 @@
+# Problema: "O triplo de um número menos 5 é igual ao dobro do mesmo número mais 1."
+# Equação: 3x - 5 = 2x + 1
+
+# Resolvendo a equação:
+# 3x - 2x = 1 + 5
+# x = 6
+
+numero = 6
+
+# Calculando cada lado da equação para verificação
+lado_esquerdo = (3 * numero) - 5
+lado_direito = (2 * numero) + 1
+
+print(f"O numero procurado (x) é {numero}")
+print(f"O lado esquerdo da expressão equivale a {lado_esquerdo}" )
+print(f"O lado direito da expressão equivale a {lado_direito}. ")
