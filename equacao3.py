d4dac52aec26161e0f569c336f2753d4e3906daa
Yaggo Rodrigues de Lima <yaggo.jose.lurdinha1982@gmail.com>
2026-08-17 20:11:05 -0300

    Atividade sobre equação - 17/08/2026.
    Atividade requisitada pelo professor Oldair Gabriel.

---
+++ equacao3.py
@@ -0,0 +1,27 @@
+# Três amigos somaram suas idades. 
+# João tem o dobro da idade de Pedro. 
+# Carlos tem a mesma idade de Pedro. 
+# A soma das idades é 60 anos. Qual é a idade de cada um?
+
+
+# Definição do problema:
+# Idade de Pedro = P
+# Idade de João = 2 * P
+# Idade de Carlos = P
+# A soma das idades é 60: P + 2P + P = 60
+
+# Resolvendo a equação:
+# 4P = 60
+# P = 60 / 4
+# P = 15
+
+idade_pedro = 15
+idade_joao = 2 * idade_pedro
+idade_carlos = idade_pedro
+
+soma_idades = idade_pedro + idade_carlos + idade_joao
+
+print(f"A idade de Pedro é {idade_pedro}. ")
+print(f"A idade de João é {idade_joao}. ")
+print(f"A idade de Carlos é {idade_pedro} ")
+print(f"A soma das idades é igual a {soma_idades}. ")
