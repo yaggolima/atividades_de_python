869d8582d76da8c2cb8d4acc8b09a63f757cff08
Yaggo Rodrigues de Lima <yaggo.jose.lurdinha1982@gmail.com>
2026-08-17 19:40:18 -0300

    Atividade de Equação - 17/08/2026

---
+++ equacao1.py
@@ -0,0 +1,13 @@
+# Descubra o número somado ao seu dobro resulta em 36." Qual é o esse número?
+
+
+# Um número somado ao seu dobro resulta em 36
+# x + 2x = 36
+# 3x = 36
+# x = 36/3
+# x + 12
+
+
+x = 12 
+resultado = x + (2 * x)
+print(f"O número é {x}, e seu dobro é {2*x}. Somados, o resultado é {resultado}.")
