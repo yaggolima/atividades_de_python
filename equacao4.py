9481807e58e74ba3940b2cd329b640161393b2dc
Yaggo Rodrigues de Lima <yaggo.jose.lurdinha1982@gmail.com>
2026-08-17 20:18:56 -0300

    Atividade sobre equção - 17/08/2026
    Atividade requisitada pelo professor Oldair Gabriel.

---
+++ equacao4.py
@@ -0,0 +1,15 @@
+# Problema: "Multiplique um número por 3 e subtraia 9. O resultado é igual ao próprio número."
+# Equação: 3x - 9 = x
+
+# Resolvendo a equação:
+# 3x - x = 9
+# 2x = 9
+# x = 9 / 2
+# x = 4.5
+
+numero = 4.5
+resultado = (3 * numero) - 9
+
+print(f"O número procurado é {numero}")
+print(f"Verificação: (3 * {numero} - 9 = {resultado}). ")
+print(f"O resultado ({resultado}) é igual ao numero ({numero}) original. ")
